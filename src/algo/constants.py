e29633ef90baa30f4e782f8cd427ab6308f912d5
Lipeng.W <wang.lipeng174@e.ntu.edu.sg>
2016-03-02 09:27:54 +0800

    UPDATE

--- src/algo/constants.py
+++ src/algo/constants.py
@@ -12,9 +12,9 @@ LEFT = "A"
 RIGHT = "D"
 FORWARD = "1" # 1..f (1-15) lower case!
 
-LA_ALIGN = "L" # L = left angle alignment
-LD_ALIGN = "Q" # Q = left distance alignment (Includes rotate left and right back)
-FA_ALIGN = "F" # F = front angle alignment
-FD_ALIGN = "W" # W = front distance alignment
+LA_AL = "L" # L = left angle alignment
+LD_AL = "Q" # Q = left distance alignment (Includes rotate left and right back)
+FA_AL = "F" # F = front angle alignment
+FD_AL = "W" # W = front distance alignment
 
 REQ_SENSOR = "E"
